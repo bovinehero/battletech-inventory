22957e7ef8c898db1668218f82d78741df5d96b5
Bovine Hero <bhero@Bovines-HeroBook-Pro.local>
2023-05-08 22:00:39 +0100

    added models to admin view

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Pilot, Mech
 
 # Register your models here.
+
+@admin.register(Pilot)
+class PilotAdmin(admin.ModelAdmin):
+    list_display = ('callsign', 'slug', 'experience', 'gunnery', 'piloting', 'status')
+    search_fields = ['callsign', 'gunnery']
+    list_filter = ('experience', )
+    prepopulated_fields = {'slug': ('callsign',)}
+    actions = ['approve_pilot', 'revoke_pilot']
+
+    def approve_pilot(self, request, queryset):
+        queryset.update(status=True)
+    
+    def revoke_pilot(self, request, queryset):
+        queryset.update(status=False)
+
+@admin.register(Mech)
+class MechAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'weight',
+        'tech_level',
+        'role',
+        'stock',
+        'record_sheet',
+        'battle_value',
+        'status'
+        ) 
+    search_fields = ['type', 'tech_level']
+    list_filter = ('tech_level', )
+    actions = ['approve_mech', 'revoke_mech']
+    prepopulated_fields = {'slug': ('name',)}
+
+    def approve_mech(self, request, queryset):
+        queryset.update(status=True)
+    
+    def revoke_mech(self, request, queryset):
+        queryset.update(status=False)
+
