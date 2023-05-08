a86d8d4d1e36f14d7ddcefac6797c3a3365cf1fa
Bovine Hero <bhero@Bovines-HeroBook-Pro.local>
2023-05-08 21:54:38 +0100

    added pilot model

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Pilot(models.Model):
+    """
+    A Pilot will have a 1 2 1 relationship with a mech
+    
+    """
+    callsign = models.CharField(max_length=50)
+    gunnery = models.IntegerField(blank=True, default=4)
+    piloting = models.IntegerField(blank=True, default=5)
+    experience = models.IntegerField(blank=True, default=5)
+    slug = models.SlugField(max_length=200, unique=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return self.callsign
