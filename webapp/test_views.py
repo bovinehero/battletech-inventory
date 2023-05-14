2ccf1a8aeb01fd1fa7aad4ed7c618d9300edeea9
Bovine Hero <bhero@Bovines-HeroBook-Pro.local>
2023-05-14 16:40:48 +0100

    adding tests for new views

--- webapp/test_views.py
+++ webapp/test_views.py
@@ -22,7 +22,6 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, 'pilots.html')
 
     def test_get_mech_detail(self):
-        pass
         mech = Mech.objects.create(
             name='testor',
             slug='testor',
@@ -45,3 +44,26 @@ class TestViews(TestCase):
         bv_mod = PilotDetail.bv_modifier(self, queryset[0].gunnery,queryset[0].piloting)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(bv_mod, 1)
+
+    def test_get_active_mech_detail(self):
+        mech = Mech.objects.create(
+            name='testm',
+            slug='testm',
+            status='1'
+        )
+        pilot = Pilot.objects.create(
+            callsign='testp',
+            slug='testp',
+            status='1'
+            )
+        activeMech = ActiveMech.objects.create(
+            name='test',
+            pilot=pilot,
+            mech=mech,
+            slug='testor',
+            status='1'
+        )
+        queryset = ActiveMech.objects.filter(name=activeMech.name)
+        self.assertEqual(queryset[0].name, activeMech.name)
+        response = self.client.get(f'/{queryset[0].slug}')
+        self.assertEqual(response.status_code, 200)
