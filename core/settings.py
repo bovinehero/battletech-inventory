10d2a1abc2c1642d378813e070424246f8457449
Bovine Hero <bhero@Bovines-HeroBook-Pro.local>
2023-05-22 15:21:38 +0100

    removing debug

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     ALLOWED_HOSTS = ["bhero-battletech-inventory.herokuapp.com", "localhost", "127.0.0.1"]
