1251af1a5c3cdb6d332c2f7b5622e7f59fbb7767
Bovine Hero <bhero@Bovines-HeroBook-Pro.local>
2023-05-22 13:34:45 +0100

    changing python runtime for heroku for testing

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = True #os.environ.get('DEBUG', False)
 
 if DEBUG:
     ALLOWED_HOSTS = ["bhero-battletech-inventory.herokuapp.com", "localhost", "127.0.0.1"]
